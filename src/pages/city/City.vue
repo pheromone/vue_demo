28c612582317b7b6cc9119a5cf7b9d34608b3770
pheromone <510738319@qq.com>
2018-07-23 15:18:12 +0800

    Revert "添加city.vue和list.vue"
    This reverts commit d8883debef6e79197e8f0ebee3b79bfdfe815028.

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,15 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
-import CityList from './components/List'
 export default {
   name: 'City',
-  components: {CityList, CitySearch, CityHeader}
+  components: {CitySearch, CityHeader}
 }
 </script>
 
