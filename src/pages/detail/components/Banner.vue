4aab671c62527177439cd91657a9578409ab75da
pheromone <510738319@qq.com>
2018-07-25 15:55:50 +0800

    详情页面添加router
    详情页面添加router

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="https://ww1.sinaimg.cn/large/0065oQSqly1ftf1snjrjuj30se10r1kx.jpg" />
+    <div class="banner-info">
+      <div class="banner-title">XXXXXXXXXXXXXXX</div>
+      <div class="banner-number">
+        <span class="iconfont banner-icon">&#xe678;</span>
+        39
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+   position :relative
+   overflow :hidden
+   height :0
+   padding-bottom :55%
+   .banner-img
+    width :100%
+   .banner-info
+    display :flex
+    position :absolute
+    left:0
+    right :0
+    bottom :0
+    line-height : .6rem
+    background-image :linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))
+    .banner-title
+      flex:1
+      font-size : .32rem
+      padding :0 .2rem
+      color :#ffffff
+     .banner-number
+      margin-top : .14rem
+      color :#ffffff
+      padding :0 .4rem
+      line-height : .32rem
+      height : .32rem
+      border-radius :.2rem
+      background : rgba(0,0,0,.8)
+      font-size :.24rem
+      .banner-icon
+        font-size :.24rem
+</style>
