87cc59743170279a2e6d21f3e4853cffd3720911
pheromone <510738319@qq.com>
2018-07-26 12:29:14 +0800

    完善详情页面
    完善详情页面:添加缓存机制keep-alive.添加动画,添加数据请求

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,27 +1,40 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img class="banner-img" src="https://ww1.sinaimg.cn/large/0065oQSqly1ftf1snjrjuj30se10r1kx.jpg" />
+      <img class="banner-img" :src="bannerImg" />
       <div class="banner-info">
-        <div class="banner-title">XXXXXXXXXXXXXXX</div>
+        <div class="banner-tittle">
+          {{this.sightName}}
+        </div>
         <div class="banner-number">
-          <span class="iconfont banner-icon">&#xe678;</span>
-          39
+          <span class="iconfont banner-icon">&#xe692;</span>
+          {{this.bannerImgs.length}}
         </div>
       </div>
     </div>
-    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    <fade-animation>
+      <common-gallary
+        :imgs="bannerImgs"
+        v-show="showGallary"
+        @close="handleGallaryClose"
+      ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
+  props: {
+    sightName: String,
+    bannerImg: String,
+    bannerImgs: Array
+  },
   data () {
     return {
-      showGallary: false,
-      imgs: ['https://ww1.sinaimg.cn/large/0065oQSqly1ftf1snjrjuj30se10r1kx.jpg', 'https://ww1.sinaimg.cn/large/0065oQSqly1ftdtot8zd3j30ju0pt137.jpg']
+      showGallary: false
     }
   },
   methods: {
@@ -32,40 +45,42 @@ export default {
       this.showGallary = false
     }
   },
-  components: {CommonGallary}
+  components: {
+    CommonGallary,
+    FadeAnimation
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   .banner
-   position :relative
-   overflow :hidden
-   height :0
-   padding-bottom :55%
-   .banner-img
-    width :100%
-   .banner-info
-    display :flex
-    position :absolute
-    left:0
-    right :0
-    bottom :0
-    line-height : .6rem
-    background-image :linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))
-    .banner-title
-      flex:1
-      font-size : .32rem
-      padding :0 .2rem
-      color :#ffffff
-     .banner-number
-      margin-top : .14rem
-      color :#ffffff
-      padding :0 .4rem
-      line-height : .32rem
-      height : .32rem
-      border-radius :.2rem
-      background : rgba(0,0,0,.8)
-      font-size :.24rem
-      .banner-icon
-        font-size :.24rem
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-tittle
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
 </style>
